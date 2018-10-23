6db4ce4b1aa694adcd4267d35bdfc05d2a4c58d8
Jimmy Lin <jlin2744@gmail.com>
2018-10-23 15:18:19 -0400

    Add files via upload

--- CBinaryTree/main.c
+++ CBinaryTree/main.c
@@ -18,7 +18,6 @@
 /*
  * Binary tree implementation 
  * from **cslibrary.stanford.edu/110/BinaryTrees,html#csoln**
- * still completing problems from site**
  */
 
 typedef struct Node{
@@ -110,6 +109,39 @@ int size(struct Node *node){
 
 int maxDepth(struct Node *node){
     
+    if(node == NULL){
+        return 0;
+    }
+    else{
+        int leftDepth = maxDepth(node->left);
+        int rightDepth = maxDepth(node->right);
+        
+        if(leftDepth > rightDepth){
+            return leftDepth + 1;
+        }
+        else 
+            return rightDepth + 1;
+    }
+    
+}
+
+int minValue(struct Node* node){
+    struct Node* current = node;
+    while(current->left != NULL){
+        current = current->left;
+    }
+    
+    return current->data;
+}
+
+void printTree(struct Node* node){
+    if(node == NULL){
+        return;
+    }
+    
+    printTree(node->left);
+    printf("%d", node->data);
+    printTree(node->right);    
 }
 
 int main(int argc, char** argv) {
